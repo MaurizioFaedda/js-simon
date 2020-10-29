946398077d91d4a4943137706dd04c8ad3de105b
Maurizio <maurizio.faedda1@gmail.com>
2020-10-29 15:30:58 +0100

    stampa in pagina numeri random, count e poi spariscono

--- main.js
+++ main.js
@@ -6,22 +6,29 @@ $(document).ready(function() {
         var numero_corrente = getRndInteger(1, 100);
         numeri_casuali.push(numero_corrente);
     }
+    $('#number-random').text(numeri_casuali);
     console.log(numeri_casuali);
 
     // Da lì parte un timer di 30 secondi.
-    var time = 30;
+    var time = 5;
     var clock = setInterval(function() {
 
         // stampo il countdown
-        console.log(time);
+        $('#countdown').text(time);
         time--;
 
+        // Dopo 30 secondi, vengono rimossi i numeri dalla pagina
         if(time < 0) {
             clearInterval(clock);
-            console.log('Hai finito il tempo per memorizzare i tuoi numeri');
+            $('#number-random').toggle();
+            $('#hide').toggle();
+            $('#alert').toggle();
+
         }
     }, 1000);
     // Dopo 30 secondi, vengono rimossi i numeri dalla pagina e l'utente deve inserire (tramite prompt) i numeri che ha visto precedentemente, uno alla volta.
+
+
     // Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
 
 })
